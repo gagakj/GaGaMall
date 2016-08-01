8e39979f989407982b0ead69bc17ad77c82259fc
jiangqqlmj <781931404@qq.com>
2016-08-01 20:21:14 +0800

    初步完成报告问题界面布局

--- App/pages/CenterContent/FeedBack.js
+++ App/pages/CenterContent/FeedBack.js
@@ -11,21 +11,37 @@ import{
     Image,
     StyleSheet,
     InteractionManager,
+    TextInput,
 } from 'react-native';
 
 import { NaviGoBack } from '../../utils/CommonUtils';
 
+var content = '';
 class FeebBack extends Component {
   constructor(props) {
       super(props);
       this.buttonBackAction=this.buttonBackAction.bind(this);   
       this.submiteFeedBack=this.submiteFeedBack.bind(this);
+      this.selectFeedBack=this.selectFeedBack.bind(this);
+      this._handleOnSelect=this._handleOnSelect.bind(this);
+      this.state={
+          selectData:"",
+      }
   }
+
+  _handleOnSelect (value) {
+    this.setState({selectData:value})
+  }
+  
   //返回
   buttonBackAction(){
       const {navigator} = this.props;
       return NaviGoBack(navigator);
   }
+  selectFeedBack(){
+
+  }
+  //提交反馈
   submiteFeedBack(){
 
   }
@@ -43,8 +59,34 @@ class FeebBack extends Component {
                        <Text style={{fontSize:18,color:'white',alignSelf:'center'}}>问题报告</Text>   
                     </View>  
                 </View>
-                <View style={{height:200,backgroundColor:'white',marginTop:10}}>
-                    
+                <View style={{backgroundColor:'white',marginTop:10}}>
+                     <View style={styles.fd_content_style}>
+                           <View style={{justifyContent:'center'}}><Text style={{marginLeft:10}}>问题类型</Text></View>
+                           <View style={styles.top_style}>
+                                <TouchableOpacity style={{marginRight:10}} onPress={()=>{this.selectFeedBack()}}>
+                                    <View style={{flexDirection:'row'}}>
+                                          <Text>新需求建议</Text>
+                                          <Image source={require('../../imgs/ic_center_feedback_arrow_down.png')}
+                                           style={styles.arrow_style}
+                                            />
+                                    </View>
+                                 </TouchableOpacity>
+                           </View>
+                     </View>
+                     <Image source={require('../../imgs/ic_large_bar.png')}/>
+                     <TextInput 
+                            style={{ fontSize: 15, textAlignVertical: 'top' }}
+                            placeholder="请写下您宝贵的意见或建议..."
+                            placeholderTextColor="#aaaaaa"
+                            underlineColorAndroid="transparent"
+                            numberOfLines={8}
+                            ref={'content'}
+                            multiline={true}
+                            autoFocus={true}
+                            onChangeText={(text) => {
+                               content = text;
+                            }}
+                      />
                 </View>
                 <View style={styles.content_style}>
                     <TouchableOpacity  onPress={()=>{this.submiteFeedBack()}}>
@@ -79,6 +121,21 @@ const styles=StyleSheet.create({
     content_style:{
         alignItems:'center',
         marginTop:10,
+    },
+    arrow_style:{
+        width:15,
+        height:15,
+        marginLeft:2,
+        marginTop:2
+    },
+    top_style:{
+        alignItems:'flex-end',
+        flex:1,
+        justifyContent:'center'
+    },
+    fd_content_style:{
+        height:40,
+        flexDirection:'row'
     }
 });
 export default FeebBack;
