01fd47d0cd9d4419413a00d336769cb197b34f6d
jiangqqlmj <781931404@qq.com>
2016-08-02 15:58:21 +0800

    初步完成首页布局

--- App/pages/Home.js
+++ App/pages/Home.js
@@ -10,9 +10,13 @@ import{
     Image,
     StyleSheet,
     TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 import {IndicatorViewPager, PagerDotIndicator} from 'rn-viewpager';
+import ShortLine from '../component/ShortLine';
 var {height, width} = Dimensions.get('window');
+var item_width = (width-1)/2;
+
 const BANNER_IMGS = [  
     require('../imgs/home/1.jpg'),  
     require('../imgs/home/2.jpg'),  
@@ -23,7 +27,11 @@ const CENTER_IMGS = [
     require('../imgs/home/img_1.png'),  
     require('../imgs/home/img_2.png'),  
     require('../imgs/home/img_3.png'),  
-    require('../imgs/home/img_4.png')  
+    require('../imgs/home/img_4.png'),
+    require('../imgs/home/img_5.png'),  
+    require('../imgs/home/img_6.png'),  
+    require('../imgs/home/img_7.png'),  
+    require('../imgs/home/img_8.png')    
 ];
 class Home extends Component {
    constructor(props) {
@@ -47,6 +55,7 @@ class Home extends Component {
   render() {
         return (
            <View style={{backgroundColor:'#f5f5f5',flex:1}}>
+              
               <View style={{height:45,backgroundColor:'black',flexDirection:'row'}}>
                 <View style={{justifyContent:'flex-start',flexDirection:'row',alignItems:'center',flex:1}}>
                      <Image source={require('../imgs/home/ic_home_top_location.png')} 
@@ -61,7 +70,7 @@ class Home extends Component {
                            style={{width:24,height:24,marginRight:8,alignItems:'center'}}/>
                 </View>
               </View>
-
+              <ScrollView style={{flex:1}} showsVerticalScrollIndicator={false}>
               <IndicatorViewPager
                     style={{height:140}}
                     indicator={this._renderDotIndicator()}
@@ -113,8 +122,102 @@ class Home extends Component {
                 </View>   
              </View>
              <View style={{marginTop:8,backgroundColor:'white'}}>
-                  <View style={{height:45,justifyContent:'center',alignItems:'center'}}><Text>推荐活动</Text></View>
-             </View>
+                  <View style={{height:40,justifyContent:'center',alignItems:'center'}}><Text>推荐活动</Text></View>
+                  <View style={{flexDirection:'row',height:70}}>
+                        <View style={{flexDirection:'row',width:item_width,marginTop:5}}>
+                              <Image source={CENTER_IMGS[4]} style={{width:66,height:47,marginLeft:20}}/>
+                              <View style={{marginLeft:10}}>
+                                  <Text>每日优惠</Text>
+                                  <Text style={{color:'#999',fontSize:13,marginTop:5}}>优惠早知道</Text>
+                              </View>
+                                                    
+                        </View>
+                        <Image source={require('../imgs/home/ic_home_shu.png')} style={{height:60,marginTop:10}}/>
+                        <View style={{flexDirection:'row',width:item_width,marginTop:8}}>
+                              <Image source={CENTER_IMGS[5]} style={{width:40,height:53,marginLeft:20}}/>
+                              <View style={{marginLeft:10}}>
+                                  <Text>充值返现</Text>
+                                  <Text style={{color:'#999',fontSize:13,marginTop:5}}>充100送50</Text>
+                              </View>
+                                                    
+                        </View>
+                  </View>
+                  <ShortLine/>
+                  <View style={{flexDirection:'row',height:70}}>
+                        <View style={{flexDirection:'row',width:item_width,marginTop:3}}>
+                              <Image source={CENTER_IMGS[6]} style={{width:50,height:67,marginLeft:20}}/>
+                              <View style={{marginLeft:10,marginTop:8}}>
+                                  <Text>评论送积分</Text>
+                                  <Text style={{color:'#999',fontSize:13,marginTop:5}}>评价免费拿积分</Text>
+                              </View>                      
+                        </View>
+                        <Image source={require('../imgs/home/ic_home_shu.png')} style={{height:60}}/>
+                        <View style={{flexDirection:'row',width:item_width,marginTop:8}}>
+                              <Image source={CENTER_IMGS[7]} style={{width:40,height:40,marginLeft:20,marginTop:5}}/>
+                              <View style={{marginLeft:10}}>
+                                  <Text>嘎嘎商城</Text>
+                                  <Text style={{color:'#999',fontSize:13,marginTop:5}}>注册嘎嘎商城</Text>
+                              </View>               
+                        </View>
+                  </View>
+                </View>
+                
+                <View style={{marginTop:8,backgroundColor:'white'}}>
+                     <View style={{height:40,justifyContent:'center',alignItems:'center'}}><Text>推荐商家</Text></View>
+                     <View style={{flexDirection:'row'}}>
+                           <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                               <TouchableOpacity>
+                                  <Image source={require('../imgs/home/img.png')} style={{width:105,height:105}}/>
+                                  <View style={{marginTop:8,justifyContent:'center',alignItems:'center'}}>
+                                        <Text>叶书 master</Text>
+                                  </View>
+                               </TouchableOpacity>
+                           </View>
+                           <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                               <TouchableOpacity>
+                                  <Image source={require('../imgs/home/img.png')} style={{width:105,height:105}}/>
+                                  <View style={{marginTop:8,justifyContent:'center',alignItems:'center'}}>
+                                        <Text>微餐咖啡</Text>
+                                  </View>      
+                               </TouchableOpacity>
+                           </View>
+                           <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                               <TouchableOpacity>
+                                  <Image source={require('../imgs/home/img.png')} style={{width:105,height:105}}/>
+                                  <View style={{marginTop:8,justifyContent:'center',alignItems:'center'}}>
+                                        <Text>多伦多海鲜自助</Text>
+                                  </View>      
+                               </TouchableOpacity>
+                           </View>
+                     </View>
+                     <View style={{flexDirection:'row',marginTop:10,paddingBottom:10}}>
+                           <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                               <TouchableOpacity>
+                                  <Image source={require('../imgs/home/img.png')} style={{width:105,height:105}}/>
+                                  <View style={{marginTop:8,justifyContent:'center',alignItems:'center'}}>
+                                        <Text>叶书 master</Text>
+                                  </View>
+                               </TouchableOpacity>
+                           </View>
+                           <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                               <TouchableOpacity>
+                                  <Image source={require('../imgs/home/img.png')} style={{width:105,height:105}}/>
+                                  <View style={{marginTop:8,justifyContent:'center',alignItems:'center'}}>
+                                        <Text>微餐咖啡</Text>
+                                  </View>      
+                               </TouchableOpacity>
+                           </View>
+                           <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                               <TouchableOpacity>
+                                  <Image source={require('../imgs/home/img.png')} style={{width:105,height:105}}/>
+                                  <View style={{marginTop:8,justifyContent:'center',alignItems:'center'}}>
+                                        <Text>多伦多海鲜自助</Text>
+                                  </View>      
+                               </TouchableOpacity>
+                           </View>
+                     </View>
+                </View>
+              </ScrollView>  
            </View>         
         );
     }
