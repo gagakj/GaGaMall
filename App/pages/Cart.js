4de4bb54383b5deef3cf48f032e6a034fa2e7eb7
jiangqqlmj <781931404@qq.com>
2016-08-05 14:41:04 +0800

    初步完成购物车布局

--- App/pages/Cart.js
+++ App/pages/Cart.js
@@ -3,15 +3,69 @@ import React, {Component} from 'react';
 import{ 
     View,
     Text,
+    TouchableOpacity,
+    Image,
+    Dimensions,
+    InteractionManager,
 } from 'react-native';
+var {height, width} = Dimensions.get('window');
+import OrderConfirm from './OrderConfirm';
 class Cart extends Component {
+    constructor(props) {
+        super(props);
+        this.topItemAction=this.topItemAction.bind(this);
+        this.payItemAction=this.payItemAction.bind(this);
+        
+    }
+    topItemAction(position){
+        if(position === 0){
+
+        }
+    }
+    //结算按钮
+    payItemAction(){
+        const {navigator} = this.props;
+        InteractionManager.runAfterInteractions(() => {
+        navigator.push({
+          component: OrderConfirm,
+          name: 'OrderConfirm'
+           });
+        });
+    }
     render() {
         return (
-             <View>
-                <View style={{height:45,backgroundColor:'black',flexDirection:'column'}}>
-                    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+             <View style={{flex:1,backgroundColor:'#f5f5f5'}}>
+                <View style={{height:45,backgroundColor:'black',flexDirection:'row'}}>
+                    <View style={{width:45,height:45}}></View>
+                    <View style={{flex:1,alignItems:'center',justifyContent:'center',}}>
                        <Text style={{fontSize:18,color:'white',alignSelf:'center'}}>购物车</Text>   
                     </View>  
+                    <View>
+                        <TouchableOpacity onPress={()=>{this.topItemAction(0)}} 
+                                          style={{width:45,height:45,justifyContent:'center',alignItems:'center'}}>
+                            <Image source={require('../imgs/cart/ic_cart_btn_delete.png')} style={{width:17,height:21}}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
+                <View style={{flex:1,justifyContent:'flex-end'}}>
+                      <View style={{backgroundColor:'white',width:width,height:40}}>
+                            <View style={{flexDirection:'row',marginLeft:15,marginTop:5}}>
+                                  <Text style={{fontSize:11,color:'black',flex:1}}>预估小计</Text>
+                                  <View style={{flex:1,alignItems:'flex-end',marginRight:15}}>
+                                        <Text style={{color:'red',fontSize:11}}>¥116</Text>
+                                  </View>
+                            </View>
+                            <View style={{marginLeft:15,marginTop:3}}>
+                                  <Text style={{color:'#777',fontSize:11}}>实际总计将以商家收据为准,为保证您的权益,请于配货员联系.</Text>
+                            </View>
+                      </View>
+                      <TouchableOpacity onPress={()=>{this.payItemAction()}}>
+                              <Image source={require('../imgs/cart/ic_cart_btn_bg.png')} 
+                                     style={{width:width,height:40,justifyContent:'center',alignItems:'center'}}>
+                                     <Text style={{color:'white',fontSize:14}}>结算</Text>
+                              </Image>
+                      </TouchableOpacity>
                 </View>
              </View>
         );
---
+++ App/pages/OrderConfirm.js
@@ -0,0 +1,52 @@
+/**
+ * 城市选择
+ */
+'use strict';
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ListView,
+} from 'react-native';
+import { NaviGoBack } from '../utils/CommonUtils';
+var {height, width} = Dimensions.get('window');
+
+class OrderConfirm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttonBackAction=this.buttonBackAction.bind(this);   
+  } 
+    //返回
+  buttonBackAction(){
+      const {navigator} = this.props;
+      return NaviGoBack(navigator);
+  }
+  render() {
+    const {navigator,route} = this.props;
+    return (
+        <View style={{backgroundColor:'#f5f5f5',flex:1}}>
+             <View style={{height:45,backgroundColor:'black',flexDirection:'row'}}>
+                <TouchableOpacity onPress={() => {this.buttonBackAction()}} 
+                   style={{width:45,height:45,justifyContent:'center',alignItems:'center'}}>
+                    <Image 
+                          style={{width:13,height:20}}
+                          source={require('../imgs/ic_center_back.png')}
+                     />
+                </TouchableOpacity>  
+                <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+                    <Text style={{fontSize:18,color:'white',alignSelf:'center'}}>订单确认</Text>   
+                </View>  
+                <View style={{width:45,height:45,}}></View>
+          </View>
+       </View>         
+    );
+  }
+}
+let styles = StyleSheet.create({
+   
+});
+export default OrderConfirm
