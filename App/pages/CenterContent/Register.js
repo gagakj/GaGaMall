84322a426bebb57e8e4f2abe21af12dad0002800
jiangqqlmj <781931404@qq.com>
2016-08-09 20:29:30 +0800

    初步完成注册界面布局搭建

--- App/pages/CenterContent/Register.js
+++ App/pages/CenterContent/Register.js
@@ -8,24 +8,36 @@ import{
     Image,
     StyleSheet,
     InteractionManager,
+    TextInput,
 } from 'react-native';
 
 import { NaviGoBack } from '../../utils/CommonUtils';
-import SettingItem from '../../component/SettingItem';
-import ShortLine from '../../component/ShortLine';
-import Language from './Language';
-import FeedBack from './FeedBack';
-import About from './About';
+import ShortLineTwo from '../../component/ShortLineTwo';
 
+var username = '';
+var password = '';
+var verifyCode = '';
 class Login extends Component {
   constructor(props) {
       super(props);
-      this.buttonBackAction=this.buttonBackAction.bind(this);      
+      this.buttonBackAction=this.buttonBackAction.bind(this);    
+      this.queryVerifyCode=this.queryVerifyCode.bind(this);
+      this.buttonChangeState=this.buttonChangeState.bind(this);
+      this.registerAction=this.registerAction.bind(this);
 }
   //返回
   buttonBackAction(){
       const {navigator} = this.props;
       return NaviGoBack(navigator);
+  }
+  buttonChangeState(){
+
+  }
+  queryVerifyCode(){
+      
+  }
+  registerAction(){
+
   }
   render() {
         return (
@@ -41,6 +53,71 @@ class Login extends Component {
                        <Text style={{fontSize:18,color:'white',alignSelf:'center'}}>注册</Text>   
                     </View>  
                 </View>
+                <View style={{backgroundColor:'white',marginTop:13}}>
+                    <View style={{flexDirection:'row',height:45,alignItems:'center'}}>
+                          <TextInput 
+                            style={{height:40,fontSize: 15,textAlign: 'left',textAlignVertical:'center',flex:1}}
+                            placeholder="请输入手机号码"
+                            placeholderTextColor="#aaaaaa"
+                            underlineColorAndroid="transparent"
+                            numberOfLines={1}
+                            ref={'username'}
+                            multiline={true}
+                            autoFocus={true}
+                            onChangeText={(text) => {
+                               username = text;
+                            }}
+                      />
+                    </View>
+                    <ShortLineTwo/>
+                    <View style={{flexDirection:'row',height:45,alignItems:'center'}}>
+                          <TextInput 
+                            style={{height:40,fontSize: 15,textAlign: 'left',textAlignVertical:'center',flex:1}}
+                            placeholder="请输入密码(6位以上字符)"
+                            placeholderTextColor="#aaaaaa"
+                            underlineColorAndroid="transparent"
+                            numberOfLines={1}
+                            ref={'username'}
+                            multiline={true}
+                            autoFocus={true}
+                            onChangeText={(text) => {
+                               username = text;
+                            }}
+                           />
+                          <TouchableOpacity onPress={() => {this.buttonChangeState()}} style={{width:45,height:45,alignItems:'center',justifyContent:'center'}}>
+                                <Image source={require('../../imgs/logre/ic_pwd_off.png')} 
+                                        style={{width:17,height:14,marginLeft:13}}/>
+                          </TouchableOpacity>
+                    </View>
+                    <ShortLineTwo/>
+                    <View style={{flexDirection:'row',height:45,alignItems:'center'}}>
+                          <TextInput 
+                            style={{height:40,fontSize: 15,textAlign: 'left',textAlignVertical:'center',flex:1}}
+                            placeholder="请输入验证码"
+                            placeholderTextColor="#aaaaaa"
+                            underlineColorAndroid="transparent"
+                            numberOfLines={1}
+                            ref={'verifyCode'}
+                            multiline={true}
+                            autoFocus={true}
+                            onChangeText={(text) => {
+                               verifyCode = text;
+                            }}
+                           />
+                          <TouchableOpacity onPress={() => {this.queryVerifyCode()}} 
+                                            style={{width:100,height:45,alignItems:'center',justifyContent:'center'}}>
+                                <Text style={{fontSize:13,color:'#777'}}>获取验证码</Text>
+                          </TouchableOpacity>
+                    </View>
+                </View>
+                <Text style={{marginTop:13,marginLeft:13,fontSize:12,color:'#777'}}>注册则视为您已同意《嘎嘎商城用户协议》</Text>
+                <TouchableOpacity onPress={() => {this.registerAction()}} 
+                                  style={{justifyContent:'center',marginTop:13,alignItems:'center'}}>
+                    <Image source={require('../../imgs/logre/ic_login_btn.png')} 
+                           style={{width:300,height:40,justifyContent:'center',alignItems:'center'}}>
+                          <Text style={{color:'white'}}>注册</Text>
+                    </Image>
+                </TouchableOpacity>
              </View>
         );
     }
